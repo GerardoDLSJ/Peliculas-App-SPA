e554e95ca40e3d55e0f1b6edab91aef0a72a6be4
CesarAM12 <cesar.almaraz19@sistemas.tecsanpedro.edu.mx>
2023-05-12 08:13:34 -0600

    Estilos-nav

--- assets/js/options.js
+++ assets/js/options.js
@@ -1,9 +1,9 @@
-const APIKEY = "eddeb7865bmshdb705aa308a1b3cp1c9b1bjsn9356ab01d92b";
+const APIKEY = "5b6bfa9d83mshf358d4d89470632p1b4b54jsn6addf7a5b37a";
 
 export const options = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": '5b6bfa9d83mshf358d4d89470632p1b4b54jsn6addf7a5b37a',
+    "X-RapidAPI-Key": APIKEY,
     "X-RapidAPI-Host": "streaming-availability.p.rapidapi.com",
   },
 };
