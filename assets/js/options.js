0cf2d120eaae2b867d319cf6c13b25c6c9e29e65
Gerardo De Los Santos Juarez <gerardo.delossantos19@sistemas.tecsanpedro.edu.mx>
2023-05-15 14:02:59 -0600

    Cambios rapidos

--- assets/js/options.js
+++ assets/js/options.js
@@ -1,4 +1,4 @@
-const APIKEY = "5b6bfa9d83mshf358d4d89470632p1b4b54jsn6addf7a5b37a";
+const APIKEY = "eddeb7865bmshdb705aa308a1b3cp1c9b1bjsn9356ab01d92b";
 
 export const options = {
   method: "GET",
